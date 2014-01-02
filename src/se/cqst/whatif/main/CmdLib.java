e329dd6c4b46179c4ad9c3345645ddf429c67da4
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2014-01-02 23:26:50 +0000

    Added Javadoc
    git-svn-id: https://cqst.svn.cloudforge.com/whatif/trunk@54 69397c0b-356f-4d35-a2df-60609b7992a0

--- src/se/cqst/whatif/main/CmdLib.java
+++ src/se/cqst/whatif/main/CmdLib.java
@@ -1,3 +1,6 @@
+/* 
+ * 
+ */
 package se.cqst.whatif.main;
 
 import java.io.IOException;
@@ -6,8 +9,21 @@ import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Scanner;
 
+/**
+ * The CmdLib class contains generic methods used throughout the project.
+ * Methods in the CmdLib class are generic in the form that they aren't explictly associated
+ * with any other class. Most methods handle data input with some kind of data validation control.
+ * 
+ */
 public class CmdLib {
 	
+	/**
+	 * Read an int from the provided Scanner-object.
+	 *
+	 * @param sc Scanner to read input from
+	 * @param prompt Prompt to show user before input
+	 * @return int
+	 */
 	public static int readInt(Scanner sc, String prompt)
 	{
 		boolean getValue = false;
@@ -28,6 +44,17 @@ public class CmdLib {
 		}
 		return value;
 	}
+	
+	
+	/**
+	 * Read an int from the provided Scanner-object within the specified min and max range
+	 *
+	 * @param sc Scanner to read input from
+	 * @param prompt Prompt to show user before input
+	 * @param min Minimum value
+	 * @param max Maximum value
+	 * @return int
+	 */
 	public static int readInt(Scanner sc, String prompt, int min, int max)
 	{
 			boolean getValue = false;
@@ -46,6 +73,13 @@ public class CmdLib {
 		return value;
 	}
 	
+	/**
+	 * Read a double from the provided Scanner-object.
+	 *
+	 * @param sc Scanner to read input from
+	 * @param prompt Prompt to show user before input
+	 * @return double
+	 */
 	public static double readDouble(Scanner sc, String prompt)
 	{
 		boolean getValue = false;
@@ -66,6 +100,16 @@ public class CmdLib {
 		}
 		return value;
 	}
+	
+	/**
+	 * Read a double from the provided Scanner-object withing the specified min and max value.
+	 *
+	 * @param sc Scanner to read input from
+	 * @param prompt Prompt to show user before input
+	 * @param min Minimum value
+	 * @param max Maximum value
+	 * @return double
+	 */
 	public static double readDouble(Scanner sc, String prompt, double min, double max)
 	{
 			boolean getValue = false;
@@ -84,10 +128,26 @@ public class CmdLib {
 		return value;
 	}
 	
+	/**
+	 * Read a String from the provided Scanner-object.
+	 *
+	 * @param sc Scanner to read input from
+	 * @param prompt Prompt to show user before input
+	 * @return String
+	 */
 	public static String readString(Scanner sc,String prompt)
 	{
 		return CmdLib.readString(sc,prompt,false);
 	}	
+	
+	/**
+	 * Read a String from the provided Scanner-object that could be empty or not.
+	 *
+	 * @param sc Scanner to read input from
+	 * @param prompt Prompt to show user before input
+	 * @param allowEmpty Allow the String to be empty
+	 * @return String
+	 */
 	public static String readString(Scanner sc,String prompt, boolean allowEmpty)
 	{
 		boolean getValue = false;
@@ -108,10 +168,27 @@ public class CmdLib {
 		return value;
 	}
 	
+	/**
+	 * Ask the user to confirm an action by answering Y or N in a prompt.
+	 *
+	 * @param Scanner to read input from
+	 * @param prompt Prompt to show user before input
+	 * @return true, if successful
+	 */
 	public static boolean confirm(Scanner sc, String prompt)
 	{
 		return CmdLib.confirm(sc,prompt,false,false);
 	}
+	
+	/**
+	 * Ask the user to confirm an action by answering Y or N in a prompt. Set a default value if no input is recieved
+	 *
+	 * @param Scanner to read input from
+	 * @param prompt Prompt to show user before input
+	 * @param useDefault Use default value
+	 * @param defaultValue Default value if no input is given
+	 * @return true, if successful
+	 */
 	public static boolean confirm(Scanner sc, String prompt, boolean useDefault, boolean defaultValue)
 	{
 		String value = null;
@@ -139,10 +216,30 @@ public class CmdLib {
 		}
 	}
 	
+	/**
+	 * Creates a block of text with a specified size fom the input. If the
+	 * input is too large, the text will be cropped; if too small it will be padded with
+	 * spaces (' ').
+	 *
+	 * @param input Text that should be resized
+	 * @param size Target size of block
+	 * @return Cropped or padded String
+	 */
 	public static String textBlock(String input,int size)
 	{
 		return CmdLib.textBlock(input,size,' ');
 	}
+	
+	/**
+	 * Creates a block of text with a specified size fom the input. If the
+	 * input is too large, the text will be cropped; if too small it will be padded with
+	 * the provided padding char.
+	 *
+	 * @param input Text that should be resized
+	 * @param size Target size of block
+	 * @param padding Char to be used as padding
+	 * @return Cropped or padded String
+	 */
 	public static String textBlock(String input,int size, char padding)
 	{
 		if(input.length() == size)
@@ -168,6 +265,12 @@ public class CmdLib {
 		}
 	}
 
+	/**
+	 * Load a Java {@link Properties} file from an {@link InputStream}.
+	 *
+	 * @param filepath InputStream containing a Java .properties file
+	 * @return properties
+	 */
 	public static Properties loadProperties(InputStream filepath)
 	{
 		Properties properties = new Properties();
@@ -183,6 +286,15 @@ public class CmdLib {
 		return properties;
 	}
 	
+	/**
+	 * Load a Java {@link Properties} file from an {@link InputStream}.
+	 * The properties are filtered with the provided String, only returning
+	 * those keys that match the filter.
+	 *
+	 * @param filepath InputStream containing a Java .properties file
+	 * @param filter Input filter. Only keys matching the filter will be included
+	 * @return Filtered properties
+	 */
 	public static Properties loadProperties(InputStream filepath, String filter)
 	{
 		Properties properties = new Properties();
@@ -200,6 +312,14 @@ public class CmdLib {
 		return filteredProperties;
 	}
 	
+	/**
+	 * Filter a {@link Properties} file with the provided with the provided String, only returning
+	 * those keys that match the filter.
+	 *
+	 * @param toFilter Properties file to filter
+	 * @param filter Input filter. Only keys matching the filter will be included.
+	 * @return Filtered properties
+	 */
 	public static Properties filterProperties(Properties toFilter, String filter)
 	{
 		Properties filteredProperties = new Properties();
@@ -213,9 +333,22 @@ public class CmdLib {
 		return filteredProperties;
 	}
 
+	/**
+	 * Return a key value from a {@link Properties} object.
+	 *
+	 * @param config Properties object
+	 * @param key Key whose value should be fetched
+	 * @return The value from the fetched key, or a message stating the value could not be found.
+	 */
 	public static String getProperty(Properties config, String key)
 	{	return config.getProperty(key, TextLib.ERR_PROPERTY_NOT_FOUND + " " + key);}
 
+	/**
+	 * Writes log outputs to {@link System.out} depending on the {@code MainProject.debug} value.  
+	 *
+	 * @param importance Severity of log entry. Could be {@code DEBUG, INFO, WARNING or ERROR}
+	 * @param message Message to be printed with the entry
+	 */
 	public static void writeLog(String importance, String message)
 	{	
 		if(importance.equalsIgnoreCase("DEBUG") && MainProject.getDebug() >= 3)

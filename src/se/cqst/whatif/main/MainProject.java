1ae0e22c20bd84ccec78446bcae10f343fe9b8f3
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2014-01-05 22:11:55 +0000

    New version 0.5
    git-svn-id: https://cqst.svn.cloudforge.com/whatif/trunk@86 69397c0b-356f-4d35-a2df-60609b7992a0

--- src/se/cqst/whatif/main/MainProject.java
+++ src/se/cqst/whatif/main/MainProject.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class MainProject {
 	
 	public static final String	SYS_NAME		=	"WhatIF";
-	public static final String 	SYS_VERSION		=	"0.4";
+	public static final String 	SYS_VERSION		=	"0.5";
 	public static final String	SYS_PHASE		=	"alpha";
 	public static final String	SYS_STRING		=	SYS_NAME + "-" + SYS_PHASE + "-" + SYS_VERSION;
 	
@@ -30,7 +30,7 @@ public class MainProject {
 		 *		2  = Show error, warning & info messages
 		 *		3  = Show error, warning, info & debug messages 
 		 */	
-		CmdLib.setDebug(3);
+		CmdLib.setDebug(1);
 		
 		Scanner sc = new Scanner(System.in);
 		boolean sysExit = false;
